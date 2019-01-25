bc23157c50e4887efbf9898d70270d715097a6a7
Fabner <fabnerp@gmail.com>
2019-01-24 22:12:41 -0200

    Inserindo diálogo do bot

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -1,7 +1,8 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Threading.Tasks;
 
 namespace SimpleBot.Logic
 {
@@ -9,7 +10,27 @@ namespace SimpleBot.Logic
     {
         public string Reply(SimpleMessage message)
         {
+            string conString = "mongodb://localhost:27017";
+            MongoClient client = new MongoClient(conString);
+
+            var db = client.GetDatabase("bot");
+            var tb = db.GetCollection<BsonDocument>("transcript");
+
+            BsonDocument bson = new BsonDocument()
+            {
+                { "id", message.Id },
+                { "nome", message.User },
+                { "mensagem", message.Text }
+            };
+
+            tb.InsertOne(bson);
+
             return $"{message.User} disse '{message.Text}";
+        }
+        public static async Task<List<BsonDocument>> GetValues(Task<IAsyncCursor<BsonDocument>> queryTask)
+        {
+            var cursor = await queryTask;
+            return await cursor.ToListAsync<BsonDocument>();
         }
 
     }
